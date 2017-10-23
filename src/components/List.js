ade74ff81ad7206c637d9d9cb71e0b4e24c2c6ef
Bryan Summers <bryan.summers@kinchipsystems.co>
2017-10-23 14:21:29 +0800

    Design of general grid layout

--- src/components/List.js
+++ src/components/List.js
@@ -6,12 +6,42 @@ class List extends Component {
   }
 
   render() {
-    return <ul> {this.props.map(this.renderItem)} </ul>;
+
+    if(this.props.documents !== undefined){
+      return (
+
+          <div className="grid article_list">
+
+
+            {this.props.documents.map(function(article, index){
+                       return <a href={article._self}>
+                         <article>
+                           <div className="photo">
+                             <img src={article.mainImage.reference}/>
+                             <div className="photo-overlay">
+                               <p>{article.heading}</p>
+                             </div>
+                           </div>
+                           <h2>{article.homepageTeaser}</h2>
+                         </article>
+
+                         </a>;
+                     })}
+          </div>
+
+
+
+
+      );
+    }
+    else{
+      return (<div>Error</div>);
+    }
+
+
   }
 
-  renderItem(name) {
-    return <li key={name}>{name}</li>;
-  }
+
 }
 
 export default List;
